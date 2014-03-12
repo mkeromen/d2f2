97bb83c8222fc6b87dd38d7557a02350d48e2cc7
mkeromen <mkeromen@absolunet.com>
2014-03-12 17:03:40 -0400

    Modification de la fonction write_app_config() dans filesystem.php

--- src/Filesystem/filesystem.php
+++ src/Filesystem/filesystem.php
@@ -1,17 +1,24 @@
 <?php
 define('APP_CONFIG', dirname(__DIR__) . '/../app/config.inc');
 
-function write_app_config() {
-    //var_dump(APP_CONFIG);
+function write_app_config($search_key, $new_value) {
+
     $config_content = file_get_contents(APP_CONFIG);
     $strip_content = str_replace(array('<?php', '?>'), '', $config_content);
     $configs = explode(';', $strip_content);
 
+    foreach($configs as $item_key => $item) {
+        if(strstr($item, $search_key) !== FALSE) {
+            $configs[$item_key] = "define('" . $search_key . "', '" . $new_value . "')";
+            array_pop($configs);
+        }
+    }
 
-    print_r($configs);
+    var_dump($configs);
 
-    /*$configs = explode(';', $config_content);
-    var_dump($configs);*/
+    $new_content = implode('', $configs);
+    var_dump($new_content);
+
 }
 
 ?>
